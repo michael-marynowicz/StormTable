87a21f72bbbbee529f9892e2ca34058e29d52fed
NadineAzebaze <53220838+NadineAzebaze@users.noreply.github.com>
2022-12-05 09:32:19 +0100

    The server can upload a file and store it in the directory files :)

--- backend/src/core/core.module.ts
+++ backend/src/core/core.module.ts
@@ -3,9 +3,10 @@ import { MeetingController } from './meeting/meeting.controller';
 import { MeetingService } from './meeting/meeting.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
+import { DocumentController } from './document/document.controller';
 
 @Module({
-  controllers: [MeetingController, UserController],
+  controllers: [MeetingController, UserController, DocumentController],
   providers: [MeetingService, UserService]
 })
 export class CoreModule {}
