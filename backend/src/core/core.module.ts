a5fc6817217ff7c3123a76f5e2a7dfe3bfe5240c
NadineAzebaze <53220838+NadineAzebaze@users.noreply.github.com>
2022-12-05 10:23:46 +0100

    Refactoring download inside document :)

--- backend/src/core/core.module.ts
+++ backend/src/core/core.module.ts
@@ -4,11 +4,10 @@ import { MeetingService } from './meeting/meeting.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { DocumentController } from './document/document.controller';
-import {DownloadController} from "./download/download/download.controller";
-import {DownloadService} from "./download/download/download.service";
+import {DocumentService} from "./document/document.service";
 
 @Module({
-  controllers: [MeetingController, UserController, DocumentController, DownloadController],
-  providers: [MeetingService, UserService, DownloadService]
+  controllers: [MeetingController, UserController, DocumentController],
+  providers: [MeetingService, UserService, DocumentService]
 })
 export class CoreModule {}
