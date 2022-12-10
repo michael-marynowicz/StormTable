8f37ebdde61eafa1e51c1d89bc34c51ec2f2523f
michael-marynowicz <michaelmarynowicz5@gmail.com>
2022-12-10 18:35:17 +0100

    now we have icons of files and pictures. And when we double click on an icon that open the file or the picture

---
+++ table/src/app/components/icon/icon.component.ts
@@ -0,0 +1,45 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {IconService} from "../../services/icon.service";
+import {Observable} from "rxjs";
+import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
+import {BrainstormElementType} from "../../models/brainstorm-element.model";
+
+@Component({
+  selector: 'app-icon',
+  templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.less']
+})
+export class IconComponent implements OnInit{
+
+  @Input() src! : string;
+  @Input() name! : string;
+
+  @Input() type! : string;
+  safeURL!: SafeResourceUrl;
+
+  loadFile : Observable<Object> | undefined
+
+  constructor(private iconcservice: IconService,private sanitizer: DomSanitizer) {
+  }
+
+
+  load(){
+    this.loadFile =this.iconcservice.load(this.src)
+  }
+
+  ngOnInit(): void {
+    this.safeURL= this.sanitizer.bypassSecurityTrustResourceUrl(this.src)
+  }
+  get defineType(){
+    console.log(this.type)
+    switch (this.type){
+      case BrainstormElementType.PICTURE: return 'picture';
+      case BrainstormElementType.PDF: return 'pdf';
+      case BrainstormElementType.TXT: return 'txt';
+      case BrainstormElementType.PPTX: return 'pptx';
+      default: return this.type;
+    }
+  }
+
+
+}
