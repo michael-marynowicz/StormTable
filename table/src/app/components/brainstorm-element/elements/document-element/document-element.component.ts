78e6a0c542f2d30ee879cfa4ca3ae291dd5415a2
NadineAzebaze <53220838+NadineAzebaze@users.noreply.github.com>
2022-12-14 08:35:02 +0100

    Update document-element.component.ts

--- table/src/app/components/brainstorm-element/elements/document-element/document-element.component.ts
+++ table/src/app/components/brainstorm-element/elements/document-element/document-element.component.ts
@@ -8,41 +8,34 @@ import WebViewer from "@pdftron/webviewer";
   styleUrls: ['./document-element.component.less']
 })
 export class DocumentElementComponent implements AfterViewInit{
-
-  constructor(private element: ElementRef) {}
-
   title = 'viewer-app';
   currentElement!: string;
   @ViewChild('viewer1')  viewerRef1!: ElementRef;
   @ViewChild('viewer2')  viewerRef2!: ElementRef;
 
   ngAfterViewInit(): void {
-
     WebViewer({
       path: '../../../../../assets/lib',
       initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf'
     }, this.viewerRef1.nativeElement).then(instance => {
-
     });
 
     WebViewer({
       path: '../../../../../assets/lib',
       initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf'
     }, this.viewerRef2.nativeElement).then(instance => {
-
     });
 
   }
 
     updateVerticalScroll(event: any): void {
-      console.log(event);
       console.log(' scrolling');
       if (this.currentElement === 'viewer2') {
       this.viewerRef1.nativeElement.scrollTop = event.target.scrollTop;
-    } else if (this.currentElement === 'viewer1') {
-      this.viewerRef2.nativeElement.scrollTop = event.target.scrollTop;
+      } else if (this.currentElement === 'viewer1') {
+        this.viewerRef2.nativeElement.scrollTop = event.target.scrollTop;
+      }
     }
-  }
 
     updateCurrentElement(element: 'viewer1' | 'viewer2') {
       this.currentElement = element;
