d1dc32f890ba3c02b5e1509ba4a0044eca4c3842
NadineAzebaze <53220838+NadineAzebaze@users.noreply.github.com>
2022-12-15 05:36:14 +0100

    cleaning code of document-element

--- table/src/app/components/brainstorm-element/elements/document-element/document-element.component.ts
+++ table/src/app/components/brainstorm-element/elements/document-element/document-element.component.ts
@@ -1,11 +1,7 @@
 
 
-import {AfterViewInit, Component, Directive, ElementRef, HostListener, Inject, Input, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import WebViewer, {UI, WebViewerInstance} from "@pdftron/webviewer";
-import {DOCUMENT} from "@angular/common";
-import {Renderer} from "@grapecity/gcpdfviewer/typings/vendor/react/react-dom";
-import {HttpContext} from "@angular/common/http";
-import addEventListener = UI.addEventListener;
 
 
 @Component({
@@ -34,7 +30,6 @@ export class DocumentElementComponent implements AfterViewInit {
     WebViewer({
       path: '../../../../../assets/lib',
       initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf',
-     // licenseKey: 'demo:1671005628767:7a96dd010300000000c2d90e683f649de4cab3ff554dae5e87a2cf16c2'
     }, this.viewerRef2.nativeElement).then(instance => {
       this.viewer2 = instance;
     });
