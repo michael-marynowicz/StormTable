1ed04b7c88edb2e0b966915520573a9a1994128c
michael-marynowicz <michaelmarynowicz5@gmail.com>
2023-01-20 19:00:03 +0100

    add number of files in the directory

--- table/src/app/components/directory/directory.component.ts
+++ table/src/app/components/directory/directory.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import DirectoryModel from "../../models/directory.model";
 import DocumentModel from "../../models/document.model";
+import SessionService from "../../services/session.service";
 
 @Component({
   selector: 'app-directory',
@@ -12,8 +13,16 @@ export class DirectoryComponent{
   @Input() directory!: DocumentModel
   @Input() miniMap!: boolean
   wantFiles:boolean=false;
+
+  constructor(private sessionService:SessionService) {
+  }
   get getDirectory(){
     return this.directory as DirectoryModel
+  }
+
+  get getFiles() {
+    const directory = this.sessionService.session?.meeting.meeting.documents.find(doc =>doc.id===this.directory.id) as DirectoryModel
+    return directory.files
   }
 
 }
